aa4654845a0bea647d613db3ca384b95a43c10c0
AAKGH <abhikulkarni20@gmail.com>
2022-07-03 18:07:33 +0530

    Changed Message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/",methods=['GET','POST'])
 
 def index ():
-    return "First Deployment"
+    return "Hi, I am first AI deployed by Abhishek ! He wants you to know he loves you..."
 
 if __name__ == "__main__":
     app.run(debug=True)
